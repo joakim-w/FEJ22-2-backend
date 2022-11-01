4ef2b060ccb29de06f26e5fe485ed561a5a32a45
Joakim <wahlstrom.joakim@gmail.com>
2022-11-01 09:52:14 +0100

    lektion 1

---
+++ lektion-1/1-hello-world/helloworld.js
@@ -0,0 +1,18 @@
+const hello = () => {
+  console.log(`Hello world`)
+}
+
+// hello();
+// hello();
+// hello();
+// hello();
+// hello();
+
+// console.log(global)
+
+// setTimeout(() => {
+//   console.log('timed out')
+// }, 3000)
+
+console.log(__dirname);
+console.log(__filename);

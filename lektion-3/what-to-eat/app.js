123c9150b67079443695d75c05d79a73ef63fffc
Joakim <wahlstrom.joakim@gmail.com>
2022-11-08 15:47:42 +0100

    lektion 3

--- lektion-3/what-to-eat/app.js
+++ lektion-3/what-to-eat/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors')
 
 const options = {
   definition: {
@@ -25,6 +26,15 @@ app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 const dishController = require('./controllers/dishController');
 const applicationsController = require('./controllers/applicationsController');
 
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, Origin, X-Requested-Width')
+//   if(req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+//   }
+//   next()
+// })
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

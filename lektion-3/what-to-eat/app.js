d3feba2147182e4302446e70a2f4fed271b07190
Joakim <wahlstrom.joakim@gmail.com>
2022-11-08 15:38:29 +0100

    lektion 3

--- lektion-3/what-to-eat/app.js
+++ lektion-3/what-to-eat/app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    servers: [
+      {
+        url: 'http://localhost:8080'
+      }
+    ],
+    info: {
+      title: 'what to eat API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./controllers/*.js'], // files containing annotations as above
+};
+
+const swaggerOptions = swaggerJsdoc(options);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 
 const dishController = require('./controllers/dishController');
 const applicationsController = require('./controllers/applicationsController');
-
-
 
 
 app.use(express.json());
--- lektion-3/what-to-eat/controllers/dishController.js
+++ lektion-3/what-to-eat/controllers/dishController.js
@@ -2,10 +2,53 @@ const router = require('express').Router();
 const dishModel = require('../models/dishModel');
 const auth = require('../middleware/auth');
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    dish:
+ *      type: object
+ *      required:
+ *        - name
+ *      properties:
+ *        _id:
+ *          type: objectId
+ *          description: Auto generated Id from mongoDb
+ *        name:
+ *          type: string
+ *          description: the dish name
+ *      example:
+ *        _id: 636a51c53fe55c427f4fbb0a
+ *        name: spaghetti
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Dishes
+ *  description: dishes API
+ */
+
 // Create
 router.post('/', auth.validateApiKey, dishModel.createNewDish);
 
 // Read
+/**
+ * @swagger
+ * /api/dishes?api_key={api_key}:
+ *  get:
+ *    summary: Returns a list of all the dishes
+ *    tags: [Dishes]
+ *    responses:
+ *      200:
+ *        description: the list
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/dish'
+ */
 router.get('/', auth.validateApiKey, dishModel.getAllDishes);
 router.get('/random', auth.validateApiKey, dishModel.getRandomDishes);
 router.get('/:id', auth.validateApiKey, dishModel.getOneDishById);
